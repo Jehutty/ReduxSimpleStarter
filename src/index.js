6bd93638a2df928618b6fcede049ee7d21d12179
jehutty <athsivridis@gmail.com>
2018-10-16 00:34:20 +0300

    "Several changes closer to a protoype"

--- src/index.js
+++ src/index.js
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
 const config = require('../config/api_keys');
 const API_KEY = config.Youtube_API_KEY;
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
+class App extends Component {
+    constructor(props){
+        super(props);
 
-    );
+        this.state = { 
+            videos: [],
+            selectedVideo: null,
+        };
+
+        YTSearch({key: API_KEY, term: 'dota 2'}, (videos) => {
+            this.setState({ 
+                videos: videos,
+                selectedVideo: videos[0]
+            });
+            console.log(videos);
+        });
+    }
+
+
+    render(){
+        return (
+            <div>
+                <SearchBar />
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList 
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+                    videos={this.state.videos} />
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(<App /> , document.querySelector('.container'));
